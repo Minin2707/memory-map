841113ac8ec94046f0f52d231b7f444a71d850f5
Minin2707 <nesta.r@yandex.ru>
2026-07-31 02:10:51 +0300

    flutter

---
+++ mobile/run-dev.ps1
@@ -0,0 +1,5 @@
+$ErrorActionPreference = "Stop"
+
+flutter run `
+  --dart-define=MM_API_BASE_URL=http://10.0.2.2:8080 `
+  --dart-define=MM_GOOGLE_SERVER_CLIENT_ID=405409932401-fich0isd82cif4jvd9k2htnr94ulgdvl.apps.googleusercontent.com
